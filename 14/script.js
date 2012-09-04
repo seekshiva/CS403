4489407b08c8e68165236616f8e73803c831f20e
Shiva Nandan <seekshiva@gmail.com>
2012-09-04 05:45:26 +0530

    That was Ex. 13

--- 14/script.js
+++ 14/script.js
@@ -1,22 +1,3 @@
-function $(el) {
-    return document.getElementById(el);
-}
-
-window.onload = function () {
-    var val = 100;
-    $("s").style.position = "absolute";
-    $("s").style.top = val + "px";
-    window.onmousemove = function (e) {
-	val += 10;
-	$("s").style.left = e.clientX + "px";
-	$("s").style.top = e.clientY + "px";
-    };
+window.onload = function() {
     
-    window.ondblclick = function () {
-	window.onmousemove = null;
-	$("s").style.display = "none";
-	$("c").style.cursor = "default";
-    };
 };
-
-
