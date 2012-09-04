b6f1a2bf26f258a65bbe85dcfd3015c087d77b34
Shiva Nandan <seekshiva@gmail.com>
2012-09-04 05:58:42 +0530

    Ex. 14

--- 14/script.js
+++ 14/script.js
@@ -1,3 +1,20 @@
+function $(el) {
+    return document.getElementById(el);
+}
+
 window.onload = function() {
-    
+    $("image").style.position = "absolute";
+    window.onmousemove = function (e) {
+	$("image").style.left = e.clientX + "px";
+	$("image").style.top = e.clientY + "px";
+    };
+
+    document.body.onclick = function (e) {
+	var msg = "Nothing now.";
+	if (e.ctrlKey) 
+	    msg = "Control: " + e;
+	if (e.shiftKey) 
+	    msg = "Shift: " + e.target;
+	alert(msg);
+    };
 };
